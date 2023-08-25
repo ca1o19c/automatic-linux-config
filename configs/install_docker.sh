11639c446f34335a92d0556471ed63a1327aaec4
Caio Antonio <caio.antonio.c@outlook.com>
2023-08-25 20:31:03 -0300

    updated docker-compose version

--- configs/install_docker.sh
+++ configs/install_docker.sh
@@ -10,6 +10,6 @@ sudo usermod -aG docker $USER
 sudo chmod 777 /var/run/docker.sock
 
 echo "Installing docker-compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.20.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
