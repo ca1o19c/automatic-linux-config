75e74953954302124c234ebc35f9519d92982d80
Caio Antonio <caio.antonio.c@outlook.com>
2024-08-24 22:28:08 -0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -12,4 +12,4 @@ sudo chmod 777 /var/run/docker.sock
 echo "Installing docker-compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.21.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+docker compose --version
